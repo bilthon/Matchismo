6ddd9e6f38d4f71375b6b77b9d182075d54f1517
Nelson Perez <nelson@Nelsons-MacBook-Pro.local>
2013-02-19 01:02:38 -0300

    Initial Commit

---
+++ Matchismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchismo
+//
+//  Created by Nelson Perez on 2/19/13.
+//  Copyright (c) 2013 Nelson Perez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Nelson Perez on 2/19/13.
+//  Copyright (c) 2013 Nelson Perez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
